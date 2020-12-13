7a838a210ab5c3589717806be2aa95ac0bfe0d5c
medcl <m@medcl.net>
2020-12-13 13:57:00 +0800

    add debug filters

---
+++ proxy/filter/debug/echo.go
@@ -0,0 +1,60 @@
+/* ©INFINI, All Rights Reserved.
+ * mail: contact#infini.ltd */
+
+package debug
+
+import (
+	"fmt"
+	"infini.sh/framework/core/param"
+	"infini.sh/framework/lib/fasthttp"
+)
+
+type EchoMessage struct {
+	param.Parameters
+}
+
+func (filter EchoMessage) Name() string {
+	return "echo"
+}
+
+func (filter EchoMessage) Process(ctx *fasthttp.RequestCtx) {
+	str := filter.GetStringOrDefault("str", ".")
+	size := filter.GetIntOrDefault("repeat", 1)
+	for i := 0; i < size; i++ {
+		ctx.WriteString(str)
+	}
+	ctx.Finished()
+}
+
+type DumpHeader struct {
+	param.Parameters
+}
+
+func (filter DumpHeader) Name() string {
+	return "dump_header"
+}
+
+func (filter DumpHeader) Process(ctx *fasthttp.RequestCtx) {
+	fmt.Println("request header:")
+	fmt.Println(ctx.Request.Header.String())
+	fmt.Println("response header:")
+	fmt.Println(ctx.Response.Header.String())
+}
+
+type DumpUrl struct {
+	param.Parameters
+}
+
+func (filter DumpUrl) Name() string {
+	return "dump_url"
+}
+
+func (filter DumpUrl) Process(ctx *fasthttp.RequestCtx) {
+	fmt.Println("request: ",ctx.Request.String())
+	fmt.Println("uri: ",string(ctx.Request.RequestURI()))
+	fmt.Println("uri: ",ctx.Request.URI().String())
+	fmt.Println("query_args: ",ctx.Request.URI().QueryArgs().String())
+	fmt.Println("query_string: ",string(ctx.Request.URI().QueryString()))
+	fmt.Println("username: ",string(ctx.Request.URI().Username()))
+	fmt.Println("password: ",string(ctx.Request.URI().Password()))
+}
--- proxy/init.go
+++ proxy/init.go
@@ -3,18 +3,20 @@ package proxy
 import (
 	"infini.sh/gateway/common"
 	"infini.sh/gateway/proxy/filter/cache"
+	"infini.sh/gateway/proxy/filter/debug"
 	"infini.sh/gateway/proxy/filter/filters"
 	"infini.sh/gateway/proxy/filter/sample"
 	"infini.sh/gateway/proxy/filter/throttle"
 	"infini.sh/gateway/proxy/output/elastic"
 	"infini.sh/gateway/proxy/output/kafka"
 	"infini.sh/gateway/proxy/output/logging"
-	"infini.sh/gateway/proxy/output/echo"
 )
 
 func Init()  {
 	common.RegisterFilter(logging.RequestLogging{})
-	common.RegisterFilter(echo.EchoDot{})
+	common.RegisterFilter(debug.EchoMessage{})
+	common.RegisterFilter(debug.DumpHeader{})
+	common.RegisterFilter(debug.DumpUrl{})
 	common.RegisterFilter(elastic.Elasticsearch{})
 	common.RegisterFilter(cache.RequestCacheGet{})
 	common.RegisterFilter(cache.RequestCacheSet{})
