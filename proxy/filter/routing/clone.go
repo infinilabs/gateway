aaac44c2937f3bbe7f84c241a2fa3d310f12c5da
medcl <m@medcl.net>
2020-12-15 23:17:35 +0800

    add continue switch to clone flow filter

--- proxy/filter/routing/clone.go
+++ proxy/filter/routing/clone.go
@@ -4,11 +4,11 @@
 package routing
 
 import (
+	log "github.com/cihub/seelog"
 	"infini.sh/framework/core/global"
 	"infini.sh/framework/core/param"
 	"infini.sh/framework/lib/fasthttp"
 	"infini.sh/gateway/common"
-	log "github.com/cihub/seelog"
 )
 
 type CloneFlowFilter struct {
@@ -20,15 +20,20 @@ func (filter CloneFlowFilter) Name() string {
 }
 
 func (filter CloneFlowFilter) Process(ctx *fasthttp.RequestCtx) {
-		flows:=filter.MustGetStringArray("flows")
-		for _,v:=range flows{
-			ctx.Resume()
-			flow:=common.MustGetFlow(v)
-			if global.Env().IsDebug{
-				log.Debugf("request [%v] go on flow: [%s] [%s]",ctx.URI().String(),v,flow.ToString())
-			}
-			flow.Process(ctx)
-			ctx.Finished()
+	flows := filter.MustGetStringArray("flows")
+	continueAfterMatch := filter.GetBool("continue", false)
+
+	for _, v := range flows {
+		ctx.Resume()
+		flow := common.MustGetFlow(v)
+		if global.Env().IsDebug {
+			log.Debugf("request [%v] go on flow: [%s] [%s]", ctx.URI().String(), v, flow.ToString())
 		}
+		flow.Process(ctx)
+	}
+
+	if continueAfterMatch {
+		ctx.Finished()
+	}
 
 }
