6f4b411261409d4d258004139eedb485b8a5e5ae
medcl <m@medcl.net>
2021-06-14 11:58:18 +0800

    update sleep filter

--- proxy/filters/throttle/sleep.go
+++ proxy/filters/throttle/sleep.go
@@ -2,9 +2,11 @@ package throttle
 
 import (
 	"infini.sh/framework/core/elastic"
+	"infini.sh/framework/core/global"
 	"infini.sh/framework/core/param"
 	"infini.sh/framework/core/rate"
 	"infini.sh/framework/lib/fasthttp"
+	log "github.com/cihub/seelog"
 	"time"
 )
 
@@ -48,7 +50,10 @@ func (filter ElasticsearchHealthCheckFilter) Process(ctx *fasthttp.RequestCtx) {
 	esName:=filter.MustGetString("elasticsearch")
 	if rate.GetRateLimiter("cluster_check_health",esName,1,1,time.Second*1).Allow(){
 		result:=elastic.GetClient(esName).ClusterHealth()
-		if result.StatusCode==200{
+		if global.Env().IsDebug{
+			log.Trace(esName,result)
+		}
+		if result.StatusCode==200||result.StatusCode==403{
 			cfg:=elastic.GetConfig(esName)
 			if cfg!=nil{
 				cfg.ReportSuccess()
