85e089f6ba48ea818903726a9d8bd25013ad5688
medcl <m@medcl.net>
2021-07-25 00:30:24 +0800

    optimize locker

--- common/util.go
+++ common/util.go
@@ -11,40 +11,45 @@ var cacheMap = map[string]map[string]string{}
 var lock sync.RWMutex
 
 func GetNodeLevelShuffleKey(cluster, nodeID string) string {
-
-	lock.Lock()
 	clusterMap, ok := cacheMap[cluster]
 	if !ok {
+		lock.Lock()
 		clusterMap = map[string]string{}
 		cacheMap[cluster] = clusterMap
+		lock.Unlock()
 	}
 
 	key, ok := clusterMap[nodeID]
 	if !ok {
+		lock.Lock()
 		key = fmt.Sprintf("%v-node-%v", cluster, nodeID)
 		clusterMap[nodeID] = key
 		cacheMap[cluster] = clusterMap
+		lock.Unlock()
 	}
-	lock.Unlock()
 
 	return key
 }
 
 func GetShardLevelShuffleKey(cluster, index string, shardID int) string {
-	lock.Lock()
+
 	clusterMap, ok := cacheMap[cluster]
 	if !ok {
+		lock.Lock()
 		clusterMap = map[string]string{}
 		cacheMap[cluster] = clusterMap
+		lock.Unlock()
 	}
 
 	shardStr := util.IntToString(shardID)
 	key, ok := clusterMap[shardStr]
 	if !ok {
+		lock.Lock()
 		key = fmt.Sprintf("%v-index-%v-%v", cluster, index, shardID)
 		clusterMap[shardStr] = key
 		cacheMap[cluster] = clusterMap
+		lock.Unlock()
 	}
-	lock.Unlock()
+
 	return key
 }
