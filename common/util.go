a9f8bfcc91c4e69ed9ebc8d594331afe23b49178
medcl <m@medcl.net>
2021-10-12 19:53:30 +0800

    fix data race

--- common/util.go
+++ common/util.go
@@ -2,54 +2,55 @@ package common
 
 import (
 	"fmt"
-	"sync"
-
-	"infini.sh/framework/core/util"
 )
 
-var cacheMap = map[string]map[string]string{}
-var lock sync.RWMutex
+//var cacheMap = map[string]map[string]string{}
+//var lock sync.RWMutex
 
 func GetNodeLevelShuffleKey(cluster, nodeID string) string {
-	clusterMap, ok := cacheMap[cluster]
-	if !ok {
-		lock.Lock()
-		clusterMap = map[string]string{}
-		cacheMap[cluster] = clusterMap
-		lock.Unlock()
-	}
+	return fmt.Sprintf("%v-node-%v", cluster, nodeID)
 
-	key, ok := clusterMap[nodeID]
-	if !ok {
-		lock.Lock()
-		key = fmt.Sprintf("%v-node-%v", cluster, nodeID)
-		clusterMap[nodeID] = key
-		cacheMap[cluster] = clusterMap
-		lock.Unlock()
-	}
-
-	return key
+	//clusterMap, ok := cacheMap[cluster]
+	//if !ok {
+	//	lock.Lock()
+	//	clusterMap = map[string]string{}
+	//	cacheMap[cluster] = clusterMap
+	//	lock.Unlock()
+	//}
+	//
+	//key, ok := clusterMap[nodeID]
+	//if !ok {
+	//	lock.Lock()
+	//	key = fmt.Sprintf("%v-node-%v", cluster, nodeID)
+	//	clusterMap[nodeID] = key
+	//	cacheMap[cluster] = clusterMap
+	//	lock.Unlock()
+	//}
+	//
+	//return key
 }
 
 func GetShardLevelShuffleKey(cluster, index string, shardID int) string {
 
-	clusterMap, ok := cacheMap[cluster]
-	if !ok {
-		lock.Lock()
-		clusterMap = map[string]string{}
-		cacheMap[cluster] = clusterMap
-		lock.Unlock()
-	}
+	return fmt.Sprintf("%v-index-%v-%v", cluster, index, shardID)
 
-	shardStr := util.IntToString(shardID)
-	key, ok := clusterMap[shardStr]
-	if !ok {
-		lock.Lock()
-		key = fmt.Sprintf("%v-index-%v-%v", cluster, index, shardID)
-		clusterMap[shardStr] = key
-		cacheMap[cluster] = clusterMap
-		lock.Unlock()
-	}
-
-	return key
+	//clusterMap, ok := cacheMap[cluster]
+	//if !ok {
+	//	lock.Lock()
+	//	clusterMap = map[string]string{}
+	//	cacheMap[cluster] = clusterMap
+	//	lock.Unlock()
+	//}
+	//
+	//shardStr := util.IntToString(shardID)
+	//key, ok := clusterMap[shardStr]
+	//if !ok {
+	//	lock.Lock()
+	//	key = fmt.Sprintf("%v-index-%v-%v", cluster, index, shardID)
+	//	clusterMap[shardStr] = key
+	//	cacheMap[cluster] = clusterMap
+	//	lock.Unlock()
+	//}
+	//
+	//return key
 }
