599cfe07ca53bf9fde481c1135b400d3e5039fde
medcl <m@medcl.net>
2022-06-19 12:08:08 +0800

    cleanup headers by default in http filter

--- proxy/output/http/http.go
+++ proxy/output/http/http.go
@@ -36,6 +36,8 @@ type HTTPFilter struct {
 	MaxRetryTimes       int `config:"max_retry_times"`
 	RetryDelayInMs      int `config:"retry_delay_in_ms"`
 
+	SkipCleanupHopHeaders bool `config:"skip_cleanup_hop_headers"`
+
 	MaxConnWaitTimeout    time.Duration `config:"max_conn_wait_timeout"`
 	MaxIdleConnDuration   time.Duration `config:"max_idle_conn_duration"`
 	MaxConnDuration       time.Duration `config:"max_conn_duration"`
@@ -89,7 +91,38 @@ func (filter *HTTPFilter) Filter(ctx *fasthttp.RequestCtx) {
 
 }
 
+// Hop-by-hop headers. These are removed when sent to the backend.
+// As of RFC 7230, hop-by-hop headers are required to appear in the
+// Connection header field. These are the headers defined by the
+// obsoleted RFC 2616 (section 13.5.1) and are used for backward
+// compatibility.
+var hopHeaders = []string{
+	"Connection",          // Connection
+	"Proxy-Connection",    // non-standard but still sent by libcurl and rejected by e.g. google
+	"Keep-Alive",          // Keep-Alive
+	"Proxy-Authenticate",  // Proxy-Authenticate
+	"Proxy-Authorization", // Proxy-Authorization
+	"Te",                  // canonicalized version of "TE"
+	"Trailer",             // not Trailers per URL above; https://www.rfc-editor.org/errata_search.php?eid=4522
+	"Transfer-Encoding",   // Transfer-Encoding
+	"Upgrade",             // Upgrade
+
+	//"Accept-Encoding",             // Disable Gzip
+	//"Content-Encoding",             // Disable Gzip
+}
+
+func cleanHopHeaders(req *fasthttp.Request) {
+	for _, h := range hopHeaders {
+		req.Header.Del(h)
+	}
+}
+
 func (filter *HTTPFilter) forward(host string, ctx *fasthttp.RequestCtx) (err error) {
+
+	if !filter.SkipCleanupHopHeaders {
+		cleanHopHeaders(&ctx.Request)
+	}
+
 	orignalHost := string(ctx.Request.URI().Host())
 	orignalSchema := string(ctx.Request.URI().Scheme())
 
