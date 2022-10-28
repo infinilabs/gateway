5009595f3d869545046a6c3269b984db8ffb1fe9
medcl <m@medcl.net>
2022-10-28 15:42:10 +0800

    remove buffer limit

--- proxy/filters/elastic/bulk_reshuffle.go
+++ proxy/filters/elastic/bulk_reshuffle.go
@@ -93,7 +93,7 @@ func NewBulkReshuffle(c *config.Config) (pipeline.Filter, error) {
 	return &runner, nil
 }
 
-var docBufferPool=  bytebufferpool.NewTaggedPool("bulk_request_docs",1,1024*1024*1024,100000)
+var docBufferPool=  bytebufferpool.NewTaggedPool("bulk_request_docs",0,0,0)
 
 func (this *BulkReshuffle) Filter(ctx *fasthttp.RequestCtx) {
 
