9cf0f55fe37feaaef5e81982a6b1bab99ec73d81
medcl <m@medcl.net>
2023-02-08 17:26:53 +0800

    add options to context_switch

--- proxy/filters/routing/context_switch.go
+++ proxy/filters/routing/context_switch.go
@@ -6,7 +6,9 @@ package routing
 import (
 	"fmt"
 	"infini.sh/framework/core/config"
+	"infini.sh/framework/core/errors"
 	"infini.sh/framework/core/pipeline"
+	"infini.sh/framework/core/util"
 	"infini.sh/framework/lib/fasthttp"
 	"infini.sh/gateway/common"
 )
@@ -14,16 +16,19 @@ import (
 type ContextSwitchFilter struct {
 	Context            string     `config:"context"`
 	ContinueAfterMatch bool       `config:"continue"`
+	SkipError          bool       `config:"skip_error"`
+	StringifyValue     bool       `config:"stringify_value"`
 	DefaultAction      string     `config:"default_action"`
 	DefaultFlow        string     `config:"default_flow"`
 	Switch             []CaseRule `config:"switch"`
 
-	cases map[interface{}]CaseRule
+	cases       map[interface{}]CaseRule
 	defaultFlow common.FilterFlow
 }
 
 type CaseRule struct {
 	Case   []interface{} `config:"case"`
+	CaseValueType string  `config:"case_value_type"`
 	Action string        `config:"action"`
 	Flow   string        `config:"flow"`
 	flow   common.FilterFlow
@@ -34,37 +39,43 @@ func (filter *ContextSwitchFilter) Name() string {
 }
 
 func (filter *ContextSwitchFilter) Filter(ctx *fasthttp.RequestCtx) {
-	if len(filter.Switch)<=0{
+	if len(filter.Switch) <= 0 {
 		return
 	}
 	if filter.Context != "" {
 		key, err := ctx.GetValue(filter.Context)
 		if err != nil {
-			panic(err)
+			if filter.SkipError {
+				return
+			}
+			panic(errors.Errorf("context_parse,url:%v,err:%v", ctx.Request.URI().String(), err))
 		}
 
-		if len(filter.cases)>0{
-			v,ok:=filter.cases[key]
-			if ok{
-				if v.Action==redirectAction{
-					if v.Flow!=""{
+		if len(filter.cases) > 0 {
+			if filter.StringifyValue {
+				key=util.ToString(key)
+			}
+			v, ok := filter.cases[key]
+			if ok {
+				if v.Action == redirectAction {
+					if v.Flow != "" {
 						v.flow.Process(ctx)
 						if !filter.ContinueAfterMatch {
 							ctx.Finished()
 						}
 					}
-				}else if v.Action==dropAction{
+				} else if v.Action == dropAction {
 					ctx.Finished()
 				}
-			}else{
-				if filter.DefaultAction==redirectAction{
-					if filter.DefaultFlow!=""{
+			} else {
+				if filter.DefaultAction == redirectAction {
+					if filter.DefaultFlow != "" {
 						filter.defaultFlow.Process(ctx)
 						if !filter.ContinueAfterMatch {
 							ctx.Finished()
 						}
 					}
-				}else if filter.DefaultAction==dropAction{
+				} else if filter.DefaultAction == dropAction {
 					ctx.Finished()
 				}
 			}
@@ -79,40 +90,50 @@ func init() {
 func NewContextSwitchFlowFilter(c *config.Config) (pipeline.Filter, error) {
 	var err error
 	runner := ContextSwitchFilter{
-		DefaultAction: redirectAction,
+		DefaultAction:  redirectAction,
+		StringifyValue: true,
 	}
 	if err := c.Unpack(&runner); err != nil {
 		return nil, fmt.Errorf("failed to unpack the filter configuration : %s", err)
 	}
 
-	runner.cases= map[interface{}]CaseRule{}
+	runner.cases = map[interface{}]CaseRule{}
 
-	for _,v:=range runner.Switch{
-		if v.Action==""{
-			v.Action=runner.DefaultAction
+	for _, v := range runner.Switch {
+		if v.Action == "" {
+			v.Action = runner.DefaultAction
 		}
 
-		if v.Action==redirectAction{
-			if v.Flow==""{
-				v.Flow=runner.DefaultFlow
+		if v.Action == redirectAction {
+			if v.Flow == "" {
+				v.Flow = runner.DefaultFlow
 			}
 		}
 
-		if v.Flow!=""{
-			v.flow, err =common.GetFlow(v.Flow)
-			if err!=nil{
+		if v.Flow != "" {
+			v.flow, err = common.GetFlow(v.Flow)
+			if err != nil {
 				panic(err)
 			}
 		}
 
-		for _,v1:=range v.Case{
-			runner.cases[v1]=v
+		for _, v1 := range v.Case {
+			if runner.StringifyValue {
+				runner.cases[util.ToString(v1)] = v
+			}else{
+				if v.CaseValueType=="int"{
+					v2:=util.InterfaceToInt(v1)
+					runner.cases[v2] = v
+				}else{
+					runner.cases[v1] = v
+				}
+			}
 		}
 	}
 
-	if runner.DefaultFlow!=""{
-		runner.defaultFlow,err=common.GetFlow(runner.DefaultFlow)
-		if err!=nil{
+	if runner.DefaultFlow != "" {
+		runner.defaultFlow, err = common.GetFlow(runner.DefaultFlow)
+		if err != nil {
 			panic(err)
 		}
 	}
