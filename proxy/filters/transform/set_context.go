c19fb3c30590cee19aa9bef731ddd60c4c498d02
medcl <m@medcl.net>
2023-03-09 21:34:00 +0800

    support context and template in set_context filter

--- proxy/filters/transform/set_context.go
+++ proxy/filters/transform/set_context.go
@@ -5,7 +5,10 @@ package transform
 
 import (
 	"fmt"
+	"io"
+
 	log "github.com/cihub/seelog"
+	"github.com/valyala/fasttemplate"
 	"infini.sh/framework/core/config"
 	"infini.sh/framework/core/pipeline"
 	"infini.sh/framework/core/util"
@@ -13,7 +16,12 @@ import (
 )
 
 type SetContext struct {
-	Context map[string]interface{} `config:"context"`
+	ContextKeyword  string                 `config:"context_keyword"`
+	VariableKeyword string                 `config:"variable_keyword"`
+	Context         map[string]interface{} `config:"context"`
+	keys            map[string]interface{}
+	valueInContext  bool
+	templates       map[string]*fasttemplate.Template
 }
 
 func (filter *SetContext) Name() string {
@@ -21,27 +29,80 @@ func (filter *SetContext) Name() string {
 }
 
 func (filter *SetContext) Filter(ctx *fasthttp.RequestCtx) {
-	if len(filter.Context) > 0 {
-		keys := util.Flatten(filter.Context, false)
-		for k, v := range keys {
-			_,err := ctx.PutValue(k, v)
+	var err error
+	if filter.keys != nil && len(filter.keys) > 0 {
+		for k, v := range filter.keys {
+			if filter.valueInContext {
+				str, ok := v.(string)
+				if ok {
+
+					if filter.templates != nil {
+						t, ok := filter.templates[str]
+						if ok {
+							if t != nil {
+								str = t.ExecuteFuncString(func(w io.Writer, tag string) (int, error) {
+									variable, err := ctx.GetValue(tag)
+									x, ok := variable.(string)
+									if ok {
+										if x != "" {
+											return w.Write([]byte(x))
+										}
+									}
+									return -1, err
+								})
+							}
+						}
+					}
+
+					//is in context
+					if util.ContainStr(str, filter.ContextKeyword) {
+						v, err = ctx.GetValue(str)
+					} else {
+						v = str
+					}
+				}
+			}
+
+			_, err = ctx.PutValue(k, v)
 			if err != nil {
-				log.Error("key:",k,",value:",v,",err:",err)
+				log.Error("key:", k, ",value:", v, ",err:", err)
 			}
 		}
 	}
 }
 
 func init() {
-	pipeline.RegisterFilterPluginWithConfigMetadata("set_context",NewSetContext,&SetContext{})
+	pipeline.RegisterFilterPluginWithConfigMetadata("set_context", NewSetContext, &SetContext{})
 }
 
 func NewSetContext(c *config.Config) (pipeline.Filter, error) {
 
-	runner := SetContext{}
+	runner := SetContext{
+		VariableKeyword: "$[[",
+		ContextKeyword:  "_ctx.",
+	}
 
 	if err := c.Unpack(&runner); err != nil {
 		return nil, fmt.Errorf("failed to unpack the filter configuration : %s", err)
+	}
+
+	if len(runner.Context) > 0 {
+		runner.templates = map[string]*fasttemplate.Template{}
+		runner.keys = util.Flatten(runner.Context, false)
+		for _, v := range runner.keys {
+			str1, ok := v.(string)
+			if ok {
+				if util.ContainStr(str1, runner.VariableKeyword) {
+					runner.valueInContext = true
+					template, err := fasttemplate.NewTemplate(str1, runner.VariableKeyword, "]]")
+					if err != nil {
+						panic(err)
+					}
+					runner.templates[str1] = template
+				}
+			}
+
+		}
 	}
 
 	return &runner, nil
