723dfa55baf3f3ee650dec68b4c7830418e6d52e
hardy <luohf@infinilabs.com>
2023-06-09 21:37:41 +0800

    fix release

--- release.sh
+++ release.sh
@@ -2,17 +2,17 @@
 
 #init
 PNAME=gateway
-STABLE=/infini/Sync/Release/$PNAME/stable
+DEST=/infini/Sync/Release/$PNAME/stable
 
 if [[ $VERSION =~ NIGHTLY ]]; then
-  exit 1
+  DEST=/infini/Sync/Release/$PNAME/snapshot
 fi
 
 for t in 386 amd64 arm64 armv5 armv6 armv7 loong64 mips mips64 mips64le mipsle riscv64 ; do
-  cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz $STABLE
+  cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz $DEST
 done
 
 for t in mac-amd64 mac-arm64 windows-amd64 windows-386 ; do
-  cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-$t.zip $STABLE
+  cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-$t.zip $DEST
 done
 
