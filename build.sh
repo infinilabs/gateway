ef37100355e58e84273a5bd1e809c9c06fa2c14f
hardy <luohf@infinilabs.com>
2023-06-12 10:33:35 +0800

    fix publish

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
  #!/bin/bash
 
 #init
-PNAME=gateway
 WORKBASE=/home/jenkins/go/src/infini.sh
 WORKDIR=$WORKBASE/$PNAME
+DEST=/infini/Sync/Release/$PNAME/stable
 
 if [[ $VERSION =~ NIGHTLY ]]; then
   BUILD_NUMBER=$BUILD_DAY
+  DEST=/infini/Sync/Release/$PNAME/snapshot
 fi
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
@@ -58,9 +59,9 @@ CMD ["/opt/$PNAME/${PNAME}-linux-$t"]
 EOF
 
   docker buildx build -t infinilabs/$PNAME-$t:latest --platform=linux/$t -o type=docker .
+  docker push infinilabs/$PNAME-$t:latest
 
   docker tag infinilabs/$PNAME-$t:latest infinilabs/$PNAME-$t:$VERSION-$BUILD_NUMBER
-  docker push infinilabs/$PNAME-$t:latest
   docker push infinilabs/$PNAME-$t:$VERSION-$BUILD_NUMBER
 done
 
@@ -72,6 +73,15 @@ docker buildx imagetools create -t infinilabs/$PNAME:latest \
 docker buildx imagetools create -t infinilabs/$PNAME:$VERSION-$BUILD_NUMBER \
     infinilabs/$PNAME-arm64:$VERSION-$BUILD_NUMBER \
     infinilabs/$PNAME-amd64:$VERSION-$BUILD_NUMBER
+
+#publish
+for t in 386 amd64 arm64 armv5 armv6 armv7 loong64 mips mips64 mips64le mipsle riscv64 ; do
+  [ -f ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz ] && cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-linux-$t.tar.gz $DEST
+done
+
+for t in mac-amd64 mac-arm64 windows-amd64 windows-386 ; do
+  [ -f ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-$t.zip ] && cp -rf ${WORKSPACE}/$PNAME-$VERSION-$BUILD_NUMBER-$t.zip $DEST
+done
 
 #git reset
 cd $WORKSPACE && git reset --hard
