717154b721c12b37380800a85b8d37f3f55d672c
medcl <m@medcl.net>
2024-02-01 11:19:51 +0800

    fix: fix rotate config usage

--- pipeline/dump_hash/dump_hash.go
+++ pipeline/dump_hash/dump_hash.go
@@ -1,18 +1,6 @@
-/*
-Copyright 2016 Medcl (m AT medcl.net)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/* Copyright © INFINI LTD. All rights reserved.
+ * Web: https://infinilabs.com
+ * Email: hello#infini.ltd */
 
 package dump_hash
 
@@ -346,7 +334,7 @@ func (processor *DumpHashProcessor) processingDocs(data []byte, outputQueueName 
 	for k, v := range docs {
 		file := path.Join(global.Env().GetDataDir(), "diff", outputQueueName+"-"+util.ToString(k))
 		processor.files.Store(file, true)
-		handler := rotate.GetFileHandler(file, rotateConfig)
+		handler := rotate.GetFileHandler(file, processor.config.RotateConfig)
 		handler.Write(v.Bytes())
 		bytebufferpool.Put("dump_hash", v)
 	}
