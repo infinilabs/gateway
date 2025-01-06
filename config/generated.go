13b2e82e9eac917033973c7d4368722cc49ff2ef
hardy <luohoufu@163.com>
2025-01-06 15:49:01 +0800

    test: add env flag

--- config/generated.go
+++ config/generated.go
@@ -1,10 +1,10 @@
 package config
 
-const LastCommitLog = "N/A"
-const BuildDate = "N/A"
+const LastCommitLog = "9d0c330a4d32ed87e484d5a960d287a45a888a66"
+const BuildDate = "2025-01-06T07:45:20Z"
 
-const EOLDate = "N/A"
+const EOLDate  = "2025-12-31T10:10:10Z"
 
-const Version = "0.0.1-SNAPSHOT"
+const Version  = "1.0.0_SNAPSHOT"
 
-const BuildNumber = "001"
+const BuildNumber  = "001"
